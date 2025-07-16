8a9ed67ebbc6c6dea93b392e599cf27fbfad9356
Scott Keller <scott.keller@inspiringapps.com>
2025-07-16 10:34:18 -0600

    first commit

---
+++ library/build.gradle.kts
@@ -0,0 +1,113 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinCocoapods)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.inspiringapps.mapboxtest.library"
+        compileSdk = 36
+        minSdk = 30
+
+        withHostTestBuilder {}
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+    }
+
+    cocoapods {
+        homepage = "MapboxMapView Library"
+        summary = "Crossplatform MapBox Map View Library"
+        version = "1.0"
+        ios.deploymentTarget = "16.0"
+
+        framework {
+            baseName = "composeApp"
+            compilerOptions.optIn.add("-Xbinary=bundleId=com.compose.cocoapod_sample")
+            isStatic = true
+        }
+
+        pod("MapboxMaps") {
+            version = "11.13.3"
+            extraOpts += listOf("-compiler-option", "-fmodules")
+        }
+
+        pod("MapboxCoreMaps") {
+            extraOpts += listOf("-compiler-option", "-fmodules")
+        }
+
+        pod("MapboxCommon") {
+            extraOpts += listOf("-compiler-option", "-fmodules")
+        }
+    }
+
+    // For iOS targets, this is also where you should
+    // configure native binary output. For more information, see:
+    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
+
+    // A step-by-step guide on how to include this library in an XCode
+    // project can be found here:
+    // https://developer.android.com/kotlin/multiplatform/migrate
+    val xcfName = "libraryKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.ui)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        androidMain {
+            dependencies {
+                implementation(compose.preview)
+                implementation(libs.mapbox.android)
+                implementation(libs.mapbox.compose)
+            }
+        }
+
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.runner)
+                implementation(libs.androidx.core)
+                implementation(libs.androidx.testExt.junit)
+            }
+        }
+
+        iosMain {
+            dependencies {}
+        }
+    }
+}
---
+++ library/src/iosMain/kotlin/com/inspiringapps/mapboxtest/library/MapboxMapView.ios.kt
@@ -0,0 +1,24 @@
+package com.inspiringapps.mapboxtest.library
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+//import androidx.compose.ui.viewinterop.UIKitView
+//import cocoapods.MapboxCommon.MBXMapboxOptions
+//import cocoapods.MapboxMaps.MapView
+import kotlinx.cinterop.ExperimentalForeignApi
+//import kotlinx.cinterop.readValue
+//import platform.CoreGraphics.CGRectZero
+
+@OptIn(ExperimentalForeignApi::class)
+@Composable
+actual fun MapboxMapView(accessToken: String, modifier: Modifier) {
+//    MBXMapboxOptions.setAccessTokenForToken(Constants.MapboxToken)
+//    MapView
+//    UIKitView(
+//        factory = {
+//            MapView(frame = CGRectZero.readValue())
+//        },
+//        modifier = modifier,
+//        update = { mapView -> }
+//    )
+}
